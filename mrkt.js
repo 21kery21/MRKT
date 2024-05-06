a5b99b636561f638b20b5242545ffc4d7b18e82f
21kery21 <158022915+21kery21@users.noreply.github.com>
2024-05-06 09:46:53 +0300

    Update mrkt.js

--- mrkt.js
+++ mrkt.js
@@ -5,3 +5,27 @@ function EItem(item) {
 function SItem(item) {
     item.style.transform = 'scale(1)';
 }
+
+
+let ItemsDiv = document.getElementById("items");
+
+if (ItemsDiv) {
+    console.log(ItemsDiv)
+
+    console.log('Поле classList', ItemsDiv.classList)
+    console.log('Поле id', ItemsDiv.id)
+    console.log('Поле clientWidth', ItemsDiv.clientWidth)
+    console.log('Поле innerHTML', ItemsDiv.innerHTML)
+
+    let num = 1
+    ttl = ["Один", "Два", "Три", "Чотири", "П'ять"]
+    const items_num = ttl.length;
+    for (let x = items_num - 5; x < 5; x++) {
+        const item_name = ttl[x]
+        ItemsDiv.innerHTML += '<div class="item">'+ item_name +'</div>';
+        num += 1
+    }
+    
+} else {
+    console.log('Блок не знайдено')
+}
